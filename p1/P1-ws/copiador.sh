dc8f92c36a0112f679b98683f8d6036470943cd3
Nombre <email@example.com>
2017-02-27 17:05:48 +0100

    [EJ6] Varios cambios para que funcione la compilacion del cliente
    * Borrado ficheros de error copiados erroneamente
    * Copiadas las Beans al cliente (tambien se usan)
    * Cambiado las IPs en build.properties
    * Actualizado el copiador (NO probado!!)

--- p1/P1-ws/copiador.sh
+++ p1/P1-ws/copiador.sh
@@ -36,8 +36,20 @@ cp_client controlador/DelPagos.java
 cp_client controlador/ServletRaiz.java
 cp_client controlador/ProcesaPago.java
 cp_server visa/dao/DBTester.java ./src/server/ssii2/visa/DBTester.java
-cp_server visa/dao/VisaDAO.java ./src/server/ssii2/visa/VisaDAOWS.java
 cp_server visa/PagoBean.java
 cp_server visa/TarjetaBean.java
 
 cp ../P1-base/postgresql-jdbc4.jar .
+
+# Cositas extras que no vienen en el enunciado
+cp_client ../P1-base/src/ssii2/visa/PagoBean.java
+cp_client ../P1-base/src/ssii2/visa/TarjetaBean.java
+find src/client/ssii2 -type f -print0 | xargs -0 sed -i 's/VisaDAO/VisaDAOWS/g'
+
+if [[ -f './src/server/ssii2/visa/VisaDAOWS.java' ]];
+	echo "$(tput setaf 1) - ACHTUNG!!! - $(tput sgr0)"
+	echo 'No se ha copiado /src/server/ssii2/visa/VisaDAOWS.java'
+	echo 'para que no se sobreescriba trabajo sin querer'
+else
+	cp_server visa/dao/VisaDAO.java ./src/server/ssii2/visa/VisaDAOWS.java
+fi
